a26c750091671cd284de8274d88b809185d5be7f
Esau Abraham <esau.seattle@gmail.com>
2021-12-26 00:28:30 -0800

    Hexagon Shape, Indice Drawing, Color Shading

--- Graphics.cpp
+++ Graphics.cpp
@@ -102,17 +102,33 @@ void Graphics::DrawTestTriangle() {
 
 	struct Vertex
 	{
-		float x;
-		float y;
+		struct {
+			float x;
+			float y;
+		} pos;
+
+		struct {
+			unsigned char r;
+			unsigned char g;
+			unsigned char b;
+			unsigned char a;
+		} color;
+		
 	};
 
 	//Create Vertex Buffer (2d Triangle)
-	const Vertex vertices[] = {
-		{ 0.0f,0.5f },
-		{ 0.5f,-0.5f },
-		{ -0.5f,-0.5f }
+	Vertex vertices[] = {
+
+		{ 0.0f,0.5f, 255, 0, 0, 0 },
+		{ 0.5f,-0.5f, 0, 255, 0, 0 },
+		{ -0.5f,-0.5f, 0, 0, 255, 0 },
+		{ -0.3f,0.3f, 0, 0, 255, 0 },
+		{ 0.3f,0.3f, 0, 255, 0, 0 },
+		{ 0.0f,-1.8f, 255, 0, 0, 0 },
+	
 	};
 
+	vertices[0].color.g = 255;
 	wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
 	D3D11_BUFFER_DESC bd = {};
 	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
@@ -129,6 +145,29 @@ void Graphics::DrawTestTriangle() {
 	const UINT stride = sizeof(Vertex);
 	const UINT offset = 0u;
 	pContext->IASetVertexBuffers(0u,1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
+
+	// Create Index Buffer (Used for reusing predfined vertices for multiple triangles)
+	const unsigned short indices[] = {
+		0,1,2,
+		0,2,3,
+		0,4,1,
+		2,1,5,
+	};
+
+	wrl::ComPtr<ID3D11Buffer> pIndexBuffer;
+	D3D11_BUFFER_DESC ibd = {};
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.Usage = D3D11_USAGE_DEFAULT;
+	ibd.CPUAccessFlags = 0u;
+	ibd.MiscFlags = 0u;
+	ibd.ByteWidth = sizeof(indices);
+	ibd.StructureByteStride = sizeof(unsigned short);
+	D3D11_SUBRESOURCE_DATA isd = {};
+	isd.pSysMem = indices;
+	GFX_THROW_INFO(pDevice->CreateBuffer(&ibd, &isd, &pIndexBuffer));
+
+	// Attach Index Buffer to Pipeline
+	pContext->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
 
 	// Create Pixel Shader
 	wrl::ComPtr<ID3D11PixelShader> pPixelShader;
@@ -150,8 +189,12 @@ void Graphics::DrawTestTriangle() {
 	// Input (vertex) layout (2d position only)
 	wrl::ComPtr<ID3D11InputLayout> pInputLayout;
 	const D3D11_INPUT_ELEMENT_DESC ied[] = {
+		// Reads positions as 2 4 byte values
 		{"POSITION", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		// Reads color as 4 byte values and normalizes them from 0 - 1
+		{"COLOR", 0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0}
 	};
+
 	GFX_THROW_INFO(pDevice->CreateInputLayout(
 		ied, (UINT)std::size(ied),
 		pBlob->GetBufferPointer(),
@@ -166,19 +209,19 @@ void Graphics::DrawTestTriangle() {
 	pContext->OMSetRenderTargets(1u, pTarget.GetAddressOf(), nullptr);
 
 	// Set primitive topology to triangle list (groups of 3 verticies make triangle)
-	pContext->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
 	// configure viewport
 	D3D11_VIEWPORT vp;
-	vp.Width = 800;
-	vp.Height = 600;
+	vp.Width = 400;
+	vp.Height = 300;
 	vp.MinDepth = 0;
 	vp.MaxDepth = 1;
-	vp.TopLeftX = 0;
-	vp.TopLeftY = 0;
+	vp.TopLeftX = 100;
+	vp.TopLeftY = 100;
 	pContext->RSSetViewports(1u, &vp);
 
-	GFX_THROW_INFO_ONLY(pContext->Draw((UINT)std::size(vertices), 0u));
+	GFX_THROW_INFO_ONLY(pContext->DrawIndexed((UINT)std::size(indices), 0u, 0u));
 }
 
 
--- VertexShader.hlsl
+++ VertexShader.hlsl
@@ -1,4 +1,13 @@
-float4 main( float2 pos : POSITION ) : SV_POSITION
-{
-	return float4(pos,0.0f,1.0f);
+struct VSOut {
+	float3 color : Color;
+	float4 pos : SV_Position;
+	
+};
+
+VSOut main( float2 pos : POSITION, float3 color: COLOR )
+{	
+	VSOut vso;
+	vso.pos = float4(pos, 0.0f, 1.0f);
+	vso.color = color;
+	return vso;
 }
